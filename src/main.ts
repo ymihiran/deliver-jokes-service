37e940ea8478ff22b4f3cf1442dd2865c2de4046
Yasantha Mihiran <89309154+ymihiran@users.noreply.github.com>
2024-11-24 20:08:27 +0530

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Deliver Jokes API')
+    .setDescription('API for delivering jokes based on type')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
